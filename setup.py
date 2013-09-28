88ea5dfefca588f65705fdfd85765474587d8475
Kyle Roux <jstacoder@gmail.com>
2013-09-27 19:43:48 -0700

    inital commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+config = {
+    'name': 'ten-thousand',
+    'version': '0.0.2',
+    'author': 'Kyle Roux',
+    'author_email': 'jstacoder@gmail.com',
+    'description': 'a dice game',
+    'long_description': open('README','r').read(),
+    'url': 'git://github.com/tzp-software/ten-thousand.git',
+        }
+setup(**config)
+
+
